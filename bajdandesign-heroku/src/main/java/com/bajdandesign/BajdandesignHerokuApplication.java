a2ee639fb20f88240936b36943b8fce21d365111
bretthanson <bretthanson@192.168.0.7>
2018-11-19 21:22:38 -0600

    20181119_2122

--- bajdandesign-heroku/src/main/java/com/bajdandesign/BajdandesignHerokuApplication.java
+++ bajdandesign-heroku/src/main/java/com/bajdandesign/BajdandesignHerokuApplication.java
@@ -5,18 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Configuration
 @SpringBootApplication
 public class BajdandesignHerokuApplication extends SpringBootServletInitializer {
-	/*
-	@GetMapping("/")
-	public String sayHi() {
-		return "Hello 123567890";
-	} */
 
 	public static void main(String[] args) {
 		SpringApplication.run(BajdandesignHerokuApplication.class, args);
