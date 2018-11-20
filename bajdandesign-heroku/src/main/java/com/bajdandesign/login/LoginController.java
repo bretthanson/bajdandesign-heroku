1b316cfa9d3aa461f86f4f9fd2e32c9c4cc65831
bretthanson <bretthanson@192.168.0.7>
2018-11-19 22:17:17 -0600

    20181119_2216

--- bajdandesign-heroku/src/main/java/com/bajdandesign/login/LoginController.java
+++ bajdandesign-heroku/src/main/java/com/bajdandesign/login/LoginController.java
@@ -3,9 +3,17 @@ package com.bajdandesign.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class LoginController {
+	
+	@GetMapping("/hello")
+	public String hello(String name, Model model) {
+
+		return "hello";
+	}
 
 	@GetMapping("/login")
 	public String greeting(String name, Model model) {
