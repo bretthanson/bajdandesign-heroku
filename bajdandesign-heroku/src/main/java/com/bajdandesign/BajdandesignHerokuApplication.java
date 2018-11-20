85fa5a91d2318972803ee9b0b7dbda082491147c
bretthanson <bretthanson@192.168.0.7>
2018-11-19 19:46:58 -0600

    20181119_1946

--- bajdandesign-heroku/src/main/java/com/bajdandesign/BajdandesignHerokuApplication.java
+++ bajdandesign-heroku/src/main/java/com/bajdandesign/BajdandesignHerokuApplication.java
@@ -2,19 +2,28 @@ package com.bajdandesign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
-public class BajdandesignHerokuApplication {
-	
+@Configuration
+@SpringBootApplication
+public class BajdandesignHerokuApplication extends SpringBootServletInitializer {
+	/*
 	@GetMapping("/")
 	public String sayHi() {
 		return "Hello 123567890";
-	}
+	} */
 
 	public static void main(String[] args) {
 		SpringApplication.run(BajdandesignHerokuApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BajdandesignHerokuApplication.class);
 	}
 }
---
+++ bajdandesign-heroku/src/main/java/com/bajdandesign/login/LoginController.java
@@ -0,0 +1,15 @@
+package com.bajdandesign.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+    public String greeting(String name, Model model) {
+		System.out.println("login");
+        return "login";
+    }
+}
