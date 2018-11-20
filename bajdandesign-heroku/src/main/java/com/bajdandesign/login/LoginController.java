3710fb5eb1926e0b2a6edde57ffb219629b9fff8
bretthanson <bretthanson@192.168.0.7>
2018-11-19 20:46:04 -0600

    20181119_2045

--- bajdandesign-heroku/src/main/java/com/bajdandesign/login/LoginController.java
+++ bajdandesign-heroku/src/main/java/com/bajdandesign/login/LoginController.java
@@ -3,22 +3,9 @@ package com.bajdandesign.login;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class LoginController {
-
-	@RequestMapping(method = RequestMethod.GET)
-	public String home(Model model) {
-		return "home";
-	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public String anotherHome(Model model) {
-		return "anotherHome";
-	}
 
 	@GetMapping("/login")
 	public String greeting(String name, Model model) {
